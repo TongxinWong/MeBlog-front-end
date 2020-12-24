a80880b250c1633eebfb83260233e1440db3f91f
TongxinWong <jue0325@gmail.com>
2020-12-24 14:18:25 +0800

    增加头像，给导航栏加上背景图片

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
   <nav>
-    <v-app-bar flat app>
+    <v-app-bar flat app hide-on-scroll src="https://i.picsum.photos/id/600/1920/1080.jpg?hmac=qChMCsuIytSAjDY6JwJ_hziMJIKuh0LzIjwZDRdGSPk">
       <v-app-bar-nav-icon class="grey--text" @click="drawer = !drawer"></v-app-bar-nav-icon>
       <v-toolbar-title class="text-uppercase grey--text">
         <span>
@@ -14,7 +14,15 @@
       </v-btn>
     </v-app-bar>
 
-    <v-navigation-drawer v-model="drawer" app class="blue-grey lighten-1">
+    <v-navigation-drawer temporary v-model="drawer" app class="blue-grey lighten-1">
+      <v-row class="mt-5 text-center">
+        <v-col>
+          <v-avatar size="100">
+            <img src="https://i.loli.net/2020/12/24/LwVrcGh1s6BqFPf.jpg" alt="Avatar">
+          </v-avatar>
+          <p class="white--text subtitle-1 mt-1">Your name</p>
+        </v-col>
+      </v-row>
       <v-list>
         <v-list-item v-for="link in links" :key="link.text" :to="link.route">
           <v-list-item-action>
